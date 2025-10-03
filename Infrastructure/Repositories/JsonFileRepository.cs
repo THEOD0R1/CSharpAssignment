ecddc5036b3ebd36d4b04b44fb77fb59cf327037
THEOD0R1 <squirrelnub@gmail.com>
2025-10-03 14:46:13 +0200

    Added IJsonFileRepository with an ReadAsync and WriteAsync method.

---
+++ Infrastructure/Repositories/JsonFileRepository.cs
@@ -0,0 +1,53 @@
+﻿using System.Text.Json;
+
+namespace Infrastructure.Repositories;
+
+public interface IJsonFileRepository
+{
+    Task WriteAsync<T>(IEnumerable<T> jsonContent, CancellationToken cancellationToken = default);
+
+    ValueTask<IReadOnlyList<T>> ReadAsync<T>(CancellationToken cancellationToken = default);
+}
+public class JsonFileRepository : IJsonFileRepository
+{
+    private readonly string _filePath;
+    private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
+    {
+        WriteIndented = true,
+    };
+    public JsonFileRepository(string fileName = "data.json")
+    {
+        string baseDirectory = AppContext.BaseDirectory;
+        string dataDirectory = Path.Combine(baseDirectory, fileName);
+        _filePath = Path.Combine(dataDirectory, fileName);
+    }
+
+    public static void EnsureInitialized(string dataDirector, string filePath)
+    {
+        if(!Directory.Exists(dataDirector))
+            Directory.CreateDirectory(dataDirector);
+
+        if (!File.Exists(filePath))
+            File.WriteAllText(filePath, "[]");
+    }
+
+    public async ValueTask<IReadOnlyList<T>> ReadAsync<T>(CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            await using FileStream stream = File.OpenRead(_filePath);
+            IReadOnlyList<T>? content = await JsonSerializer.DeserializeAsync<IReadOnlyList<T>>(stream, _jsonOptions, cancellationToken);
+            return content ?? [];
+        }
+        catch
+        {
+            return [];
+        }
+    }
+
+    public async Task WriteAsync<T>(IEnumerable<T> products, CancellationToken cancellationToken = default)
+    {
+        await using FileStream stream = File.Create(_filePath);
+        await JsonSerializer.SerializeAsync(stream, products, _jsonOptions, cancellationToken);
+    }
+}
