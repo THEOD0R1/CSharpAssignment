11c54bf454c6a2cb776c851db40e98a7e4b1523c
THEOD0R1 <squirrelnub@gmail.com>
2025-10-17 15:05:55 +0200

    Adopt MVVM pattern with CommunityToolkit.Mvvm
    Updated App.xaml to remove StartupUri and added an empty.
    Added CommunityToolkit.Mvvm package to the project to enable
    MVVM utilities. Introduced MainViewModel class with an observable
    property to manage the current view model dynamically.

---
+++ Presentation.WpfApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,9 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Presentation.WpfApp.ViewModels;
+
+public class MainViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private ObservableObject _currentViewModel = null!;
+}
